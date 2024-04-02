986f2135c35bb0f62831edb215ea018ea0e08947
sathish kumar <sathishkumar172003@gmail.com>
2024-04-02 14:08:52 +0530

    Forkfiy 1.1

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = 'd72153de-d7b0-4d54-8ffd-d875aa4119a1';
+export const TIMEOUT_SEC = 10;
+export const RESULTS_PER_PAGE = 10;
+export const MODEL_TIMEOUT_SEC = 2.5;
